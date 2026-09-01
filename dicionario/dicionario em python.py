b21559ddcdb4c0d25bda30609a52d714c31bfbea
codigos de exercicios. Python <mateusluan900@gmail.com>
2026-09-01 15:54:51 -0400

    Adicionando exercício jogo de dado em python

---
+++ dicionario/dicionario em python.py
@@ -0,0 +1,12 @@
+aluno = dict()
+aluno['nome'] = str(input("nome: "))
+aluno['media'] = float(input(f"média do {aluno["nome"]} "))
+if aluno['media'] >= 7:
+    aluno['situação'] = 'Aprovado'
+elif 5 <= aluno['media'] < 7:
+    aluno['situação'] = 'Recuperação'
+else:
+    aluno['situação'] = 'Reprovado'
+for k, v in aluno.items():
+    print(f"{k} é igual a {v}")
+
